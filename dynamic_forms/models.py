69b6b404caeccedd3a2f61c3768efb25255d57af
amirkian <amir64061111@gmail.com>
2024-10-13 23:42:40 +0330

    initial project and create models

---
+++ dynamic_forms/models.py
@@ -0,0 +1,95 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+
+class Category(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="categorys")
+    name  = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name + " - " + self.owner.username
+
+
+class Form(models.Model):
+    owner  = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_forms")
+    title  = models.CharField(max_length=255)
+    position = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.title + " - " + self.owner.username
+
+
+class Process_Type(models.TextChoices):
+    LINEAR = 'linear'
+    FREE = 'free'
+
+class Process(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_process")
+    title  = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, null=True,
+        related_name="category_process"
+    )
+    forms = models.ManyToManyField("Form", related_name="form_process")
+    type = models.CharField(max_length=10, choices=Process_Type.choices, default=Process_Type.FREE)
+    is_public = models.BooleanField(default=True)
+    password = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.title + " - " + self.owner.username
+
+
+class Question(models.Model):
+    TEXT = 'text'
+    SELECT = 'select'
+    CHECKBOX = 'checkbox'
+
+    QUESTION_TYPES = [
+        (TEXT, 'Text'),
+        (SELECT, 'Select'),
+        (CHECKBOX, 'Checkbox'),
+    ]
+
+    form  = models.ForeignKey(Form, on_delete=models.CASCADE, related_name='questions')
+    title = models.TextField()
+    type  = models.CharField(max_length=10, choices=QUESTION_TYPES)
+
+    def __str__(self) -> str:
+        return self.title + " - " + self.form.__str__()
+
+
+class SelectChoice(models.Model):
+    select_question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="select_choices")
+    title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Response(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_responses')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question_responses')
+    answer = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.question.title + " - " + self.user.username
+
+
+class WatchFormHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_watch_histories')
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name='form_watch_histories')
+    watched_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.form.title + " - " + self.user.username
+
+
+
+
+
+# class Response2(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_responses2')
+#     form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name='form_responses')
+#     answers = models.JSONField()
